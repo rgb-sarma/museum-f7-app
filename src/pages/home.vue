62100c6028f0e1762e47a54fa878629d91b090f7
Vidak i guess <vidak.kozomara@gmail.com>
2022-09-11 16:39:54 +0200

    test

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,15 +1,12 @@
 <template>
   <f7-page name="home">
     <!-- Top Navbar -->
-    <f7-navbar :sliding="true">
+    <f7-navbar>
       <f7-nav-left>
         <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="left"></f7-link>
       </f7-nav-left>
       <f7-nav-title sliding>Museum App</f7-nav-title>
-      <f7-nav-right>
-        <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
-      </f7-nav-right>
-      <f7-nav-title-large>Museum App</f7-nav-title-large>
+      <!-- <f7-nav-title-large>Museum App</f7-nav-title-large> -->
     </f7-navbar>
     <!-- Toolbar-->
     <f7-toolbar bottom>
@@ -36,46 +33,28 @@
       <f7-list-item link="/form/" title="Form"></f7-list-item>
     </f7-list>
 
-    <f7-block-title>Modals</f7-block-title>
-    <f7-block strong>
-      <f7-row>
-        <f7-col width="50">
-          <f7-button fill raised popup-open="#my-popup">Popup</f7-button>
-        </f7-col>
-        <f7-col width="50">
-          <f7-button fill raised login-screen-open="#my-login-screen">Login Screen</f7-button>
-        </f7-col>
-      </f7-row>
-    </f7-block>
 
-    <f7-block-title>Panels</f7-block-title>
-    <f7-block strong>
-      <f7-row>
-        <f7-col width="50">
-          <f7-button fill raised panel-open="left">Left Panel</f7-button>
-        </f7-col>
-        <f7-col width="50">
-          <f7-button fill raised panel-open="right">Right Panel</f7-button>
-        </f7-col>
-      </f7-row>
-    </f7-block>
-
-    <f7-list>
-      <f7-list-item
-        title="Dynamic (Component) Route"
-        link="/dynamic-route/blog/45/post/125/?foo=bar#about"
-      ></f7-list-item>
-      <f7-list-item
-        title="Default Route (404)"
-        link="/load-something-that-doesnt-exist/"
-      ></f7-list-item>
-      <f7-list-item
-        title="Request Data & Load"
-        link="/request-and-load/user/123456/"
-      ></f7-list-item>
-    </f7-list>
   </f7-page>
 </template>
+
+<script>
+  import {  } from 'vue'
+  export default {
+    props: {
+      f7route: Object,
+      f7router: Object,
+    },
+    setup(p) {
+      // console.log(p.f7route);
+      // console.log(p.f7router);
+
+      
+      return {
+      }
+    }
+  }
+</script>
+
 <style scoped lang="scss">
 .item-wrap {
   display: flex;
